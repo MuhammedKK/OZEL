fabf3c314eca74872cbc1035bd1f2a34b08a7e5c
MuhammedKK <khaloood2281999@gmail.com>
2020-05-21 11:18:00 -0700

    Latest Modified

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -1,5 +1,28 @@
 $(function () {
 
+
+    /*-------------------------------------
+    Change Selected On Pagination On Click
+    -------------------------------------*/
+
+    $('.pages .pagination li a').click(function () {
+
+        $(this).parent().addClass('selected').siblings().removeClass('selected');
+
+    });
+
+    /*-------------------------------------
+    Search popup
+    -------------------------------------*/
+
+    $('.fa-search').on('click', function () {
+        $('.popup').fadeIn(400);
+    });
+
+    $('.closebtn').on('click', function () {
+        $('.popup').fadeOut(400);
+    });
+
     /*-------------------------------------
     Loading Page
     -------------------------------------*/
@@ -26,20 +49,31 @@ $(function () {
     Animate Menu Bar
     -------------------------------------*/
 
-    let menu = $('i.fa-bars');
-    let navLinks = $('.nav-link');
+    let menu = $('nav i.fa-bars');
+    let navLinks = $('.links');
     menu.click(function () {
-        if($(this).hasClass('open')) {
-            $(this).removeClass('fa-bars open');
-            $(this).addClass('fa-times');
-            navLinks.slideToggle(300);
+        if($(window).width() > 767) {
+            if($(this).hasClass('open')) {
+                $(this).removeClass('fa-bars open');
+                $(this).addClass('fa-times');
+                navLinks.slideToggle(300);
+            } else {
+                $(this).removeClass('fa-times');
+                $(this).addClass('fa-bars open');
+                navLinks.slideToggle(300);
+            }       
         } else {
-            $(this).removeClass('fa-times');
-            $(this).addClass('fa-bars open');
-            navLinks.slideToggle(300);
+            $('.links-open').animate({
+                right: '0'
+            }, 400);
+
+            $('.links-open .fa-times').click(function() {
+                $(this).parent().animate({
+                    right: '-100%'
+                }, 400);
+            });
         }
     });
-
 
     /*-------------------------------------
     Animate Search Bar
@@ -60,8 +94,8 @@ $(function () {
         slidesToShow: 6,
         autoplay:true,
         slidesToScroll: 1,
-        prevArrow: '.fa-chevron-left',
-        nextArrow: '.fa-chevron-right ',
+        prevArrow: ' .left',
+        nextArrow: ' .right',
         responsive: [
             {
             breakpoint: 1024,
@@ -132,4 +166,120 @@ $(function () {
         ]
     });
 
+    $('.testi-boxes').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay:true,
+        arrows:false,
+        responsive: [
+            {
+            breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+            breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.posts').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay:true,
+        arrows:false,
+        responsive: [
+            {
+            breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+            breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.exp-slides').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay:true,
+        arrows:false,
+        responsive: [
+            {
+            breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+            breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
